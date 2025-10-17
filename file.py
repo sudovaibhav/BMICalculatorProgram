4a5df371052a9b5cc1a7a4f9dc347acc17f968d1
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-10-17 22:03:18 +0530

    Added BMI Category

--- file.py
+++ file.py
@@ -1,5 +1,15 @@
 weight = int(input("Enter the weight in kg : "))
 height = int(input("Enter the heigh in meter: "))
-bmi = weight / height
+bmi = weight / (height ** 2)
 
 print(f"BMI of {weight}kg and {height}m is  {bmi}")
+
+if (bmi >= 18 and bmi <= 25):
+    print("Category: Normal")
+elif (bmi < 18):
+    print("Category: Underweight")
+elif (bmi > 25):
+    print("Category: Overweight")
+else: 
+    print("Error")
+    
